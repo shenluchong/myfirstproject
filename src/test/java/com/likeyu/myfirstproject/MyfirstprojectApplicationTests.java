9442a95783cc8917d7ae0c3b50aa388dee3f9325
luchong <415042766@qq.com>
2019-02-18 20:42:43 +0800

    first commit

---
+++ src/test/java/com/likeyu/myfirstproject/MyfirstprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.likeyu.myfirstproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfirstprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
